4ec4fa7d0f044a25f31cbe3da1a90d1610a94d76
Santos Villada <scvillada@gmail.com>
2022-08-09 13:22:09 -0300

    entrega lista

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 //debugger
+/*
 function eleccionProducto() {
     let presentar = confirm("Tiene los siguientes productos a elegir: blanco, negro y gris.")
     let eleccion = prompt("Indica que producto vas a llevar")
@@ -18,4 +19,38 @@ function presupuesto(color) {
             return "No tenemos ese producto disponible"
     }
 }
-eleccionProducto ()
+eleccionProducto () */
+
+//debugger
+const celulares = []
+const carrito = []
+const celular = [iPhonex, iPhone11, iPhone12, iPhone13]
+const iPhonex = {
+    memoria: "350gb",
+    bateria: "%93",
+    color: "negro",
+}
+class producto {
+    constructor(id, descripcion, importe) {
+    this.id = id
+    this.descripcion = descripcion
+    this.importe = importe
+    }
+}
+
+function creoID() {
+    return parseInt(Math.random() * 100000)
+}
+
+function agregarProductos() {
+    debugger
+    let id = creoID()
+    let descripcion = prompt("Ingresa el nombre del Producto:")
+    let importe = parseInt(prompt("Ingresa el importe:"))
+    celulares.push(new producto(id, descripcion, importe))
+    console.table(celulares)
+}
+agregarProductos()
+
+
+
