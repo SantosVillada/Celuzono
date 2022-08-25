5af158bd6af6e363981cd5a5447e221fc6be3d01
Santos Villada <scvillada@gmail.com>
2022-08-24 21:41:00 -0300

    segunda entrega

--- js/appceluzono.js
+++ js/appceluzono.js
@@ -1,3 +1,8 @@
+const divCelulares = document.querySelector(".divCelulares");
+const divCarrito = document.querySelector(".carrito");
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+
+
 const productos = [{
         nombre: "iPhonex",
         precio: "800",
@@ -20,7 +25,7 @@ function creoID() {
     return parseInt(Math.random() * 100000)
 }
 
-const divCelulares = document.querySelector(".divCelulares");
+
 
 function creoCards() {
     productos.forEach((prod) => {
@@ -28,7 +33,7 @@ function creoCards() {
         <h3>${prod.nombre}</h3>
         <p>$${prod.precio}</p>
         <p>${prod.id}</p>
-        <button id="btn-agregar${prod.id}">Agregar producto</button>
+        <button id="btn-agregar${prod.id}">Comprar</button>
         </div>`;
     });
     funcionBtn()
@@ -37,21 +42,50 @@ function creoCards() {
 function funcionBtn() {
     productos.forEach((prod) => {
         document.querySelector(`#btn-agregar${prod.id}`).addEventListener("click", () => {
-            console.log(prod)
+            console.log(prod);
+            agregarAlCarrito(prod)
         })
     })
 }
 
 function agregarAlCarrito(prod) {
     let cantidad = carrito.some((cantidad) => cantidad.id === prod.id);
-    if(cantidad === false) {
-     prod.cant = 1;
-     carrito.push(prod);
+    if (cantidad === false) {
+        prod.cant = 1;
+        carrito.push(prod);
     } else {
+        let buscarProd = carrito.find(buscarProd => buscarProd.id === prod.id)
         prod.cant++;
     }
     //productos.push(prod);
     console.log(carrito);
+    crearCarrito()
+}
+
+function crearCarrito() {
+    divCarrito.innerHTML = ""
+    carrito.forEach(prod => {
+        divCarrito.innerHTML += `<div>
+        <h3>${prod.nombre}</h3>
+        <p>$${prod.precio}</p>
+        <p>${prod.id}</p>
+        <button id="btn-borrar${prod.id}">Quitar producto</button>
+        </div>`
+    });
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    borrarProducto()
+}
+
+function borrarProducto() {
+    carrito.forEach((prod) => {
+        document
+            .querySelector(`#btn-borrar${prod.id}`)
+            .addEventListener("click", () => {
+                carrito = carrito.filter((prodfilter) => prodfilter.id !== prod.id);
+                crearCarrito()
+            });
+    })
 }
 
 creoCards();
+crearCarrito();
